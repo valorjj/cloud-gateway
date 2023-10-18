c9174912e5b7251747c57d1be54b8c8e9bb69667
Jeongjin Kim <valorjj@gmail.com>
2023-10-19 03:35:48 +0900

    1

--- src/main/java/com/example/cloudgateway/controller/AuthenticationController.java
+++ src/main/java/com/example/cloudgateway/controller/AuthenticationController.java
@@ -4,7 +4,9 @@ import com.example.cloudgateway.model.AuthenticateResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
@@ -16,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Objects;
 
 @RestController
@@ -30,25 +31,35 @@ public class AuthenticationController {
     public ResponseEntity<?> login(
         @AuthenticationPrincipal OidcUser oidcUser,
         @RegisteredOAuth2AuthorizedClient("okta") OAuth2AuthorizedClient client,
-        Model model
-    ) throws URISyntaxException {
-
-//        AuthenticateResponse authenticateResponse = AuthenticateResponse.builder()
-//            .userId(oidcUser.getEmail())
-//            .accessToken(client.getAccessToken().getTokenValue())
-//            .refreshToken(Objects.requireNonNull(client.getRefreshToken()).getTokenValue())
-//            .expiresAt(Objects.requireNonNull(client.getAccessToken().getExpiresAt()).getEpochSecond())
-//            .authorityList(oidcUser.getAuthorities()
-//                .stream()
-//                .map(GrantedAuthority::getAuthority)
-//                .toList()
-//            )
-//            .build();
+        Model model,
+        ServletServerHttpResponse response
+    ) {
+        AuthenticateResponse authenticateResponse = AuthenticateResponse.builder()
+            .userId(oidcUser.getEmail())
+            .accessToken(client.getAccessToken().getTokenValue())
+            .refreshToken(Objects.requireNonNull(client.getRefreshToken()).getTokenValue())
+            .expiresAt(Objects.requireNonNull(client.getAccessToken().getExpiresAt()).getEpochSecond())
+            .authorityList(oidcUser.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList()
+            )
+            .build();
 
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(URI.create(FRONT_SERVER_URL));
 
-        return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).headers(headers).build();
+//        ResponseCookie cookie = ResponseCookie.from("access-token", authenticateResponse.accessToken())
+//            // 다른 엔드포인트 허용
+//            .path("/")
+//            // https 만 쿠키를 주고 받는다는 것이다.
+//            .secure(false)
+//            .sameSite("Lax")
+//            .httpOnly(false)
+//            .build();
+
+
+        return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).headers(headers).body(authenticateResponse);
     }
 
 }
