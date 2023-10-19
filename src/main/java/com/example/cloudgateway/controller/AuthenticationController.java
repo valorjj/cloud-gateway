3c55dabc8750a6899e9901b988c0c20004ca5228
Jeongjin Kim <valorjj@gmail.com>
2023-10-19 15:39:47 +0900

    1

--- src/main/java/com/example/cloudgateway/controller/AuthenticationController.java
+++ src/main/java/com/example/cloudgateway/controller/AuthenticationController.java
@@ -2,9 +2,6 @@ package com.example.cloudgateway.controller;
 
 import com.example.cloudgateway.model.AuthenticateResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.security.core.GrantedAuthority;
@@ -17,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
 import java.util.Objects;
 
 @RestController
