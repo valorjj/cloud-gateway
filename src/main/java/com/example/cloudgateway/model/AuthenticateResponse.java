97a54e620e38877431e702091e4299174d0c9aab
Jeongjin Kim <valorjj@gmail.com>
2023-10-26 20:53:10 +0900

    1

--- src/main/java/com/example/cloudgateway/model/AuthenticateResponse.java
+++ src/main/java/com/example/cloudgateway/model/AuthenticateResponse.java
@@ -1,10 +1,13 @@
 package com.example.cloudgateway.model;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 
 import java.util.Collection;
 
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record AuthenticateResponse(
     String userId,
     String accessToken,
