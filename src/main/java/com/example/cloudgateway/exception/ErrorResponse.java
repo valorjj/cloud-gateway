efb3c78f87d6843f8b0557ad31b96d185752223b
Jeongjin Kim <valorjj@gmail.com>
2023-10-26 20:52:17 +0900

    1

--- src/main/java/com/example/cloudgateway/exception/ErrorResponse.java
+++ src/main/java/com/example/cloudgateway/exception/ErrorResponse.java
@@ -1,11 +1,14 @@
 package com.example.cloudgateway.exception;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record ErrorResponse(
     String errorMessage,
-    int errorCode
+    Integer errorCode
 
 ) {
 }
